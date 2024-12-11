36a5fa67152c0b048b2365152d4c843aef97ad17
Anupa <anupa@macbookpro.home>
2024-12-11 13:50:13 -0500

    Part 2: Adding inheritance

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //setting up classes
 class ProductProperties {
-    //intitalize the properties
+    //inititalize the properties
     constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
@@ -16,7 +16,31 @@ class ProductProperties {
     }
 }
 
+//list product
 const product = new ProductProperties("Apple", 2.5, 50);
 
+//output
 console.log(product.getTotalValue());
 console.log(product.toString());
+
+//adding inheritance
+class PerishableProductProperties extends ProductProperties {
+    //initialize
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    //override
+    toString() {
+        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+    }
+}
+
+//list perishable
+const milk = new PerishableProductProperties("Milk", 1.5, 10, "2025-01-10");
+const bread = new PerishableProductProperties("Bread", 1.5, 2, "2024-12-30");
+
+//output
+console.log(milk.toString());
+console.log(bread.toString());
